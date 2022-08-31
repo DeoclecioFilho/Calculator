4e3b9220609814c80f02fe64496317f86d0da9f5
Deoclécio Filho <deoclecio.filho@estudante.ufjf.br>
2022-08-31 18:52:16 -0300

    Inserido lógica

--- src/components/Display.js
+++ src/components/Display.js
@@ -6,18 +6,17 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 20,
         justifyContent: 'center',
-        backgroundColor: 'rgb(0,0,0,0.6)',
+        backgroundColor: 'rgba(0,0,0,0.6)',
         alignItems: 'flex-end',
     },
     displayValue:{
         fontSize: 60,
-        color: '#fff'
+        color: '#fff',
     }
 })
-export default props => {
+export default props => 
     <View style={styles.display}>
         <Text style={styles.displayValue} numberOfLines={1}>
-            {props.displayValue}
+            {props.value}
         </Text>
     </View>
-}
